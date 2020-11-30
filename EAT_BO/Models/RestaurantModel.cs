d81617b870721e9a88e0b42a6da2f6e4a8a94ea9
heitor <heitor.95moniz19@gmail.com>
2020-11-30 20:32:51 +0000

    Add project files.

---
+++ EAT_BO/Models/RestaurantModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EAT_BO.Models
+{
+    public class RestaurantModel
+    {
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string address { get; set; }
+        public string tag { get; set; }
+        public List<DrinksDessertModel> drinks { get; set; }
+        public List<DrinksDessertModel> desserts { get; set; }
+        public List<MenuModel> menu { get; set; }
+    }
+    public class FirebaseRestaurantModel
+    {
+        
+        public string name { get; set; }
+        public string email { get; set; }
+        public string address { get; set; }
+        public string image_url { get; set; }
+        public string tag { get; set; }
+        public string themeColor { get; set; }
+        public DateTime time { get; set; }
+        public Dictionary<string, DrinksDessertModel> drinks { get; set; }
+        public Dictionary<string, DrinksDessertModel> desserts { get; set; }
+        public Dictionary<string, MenuModel> menu { get; set; }
+    }
+    public class DrinksDessertModel
+    {
+        public string name { get; set; }
+        public bool isAvailable { get; set; }
+    }
+    public class MenuModel
+    {
+        public string name { get; set; }
+        public double price { get; set; }
+        public string tag { get; set; }
+        public bool isAvailable { get; set; }
+        public double time { get; set; }
+        public string image_url { get; set; }
+    }
+}
---
+++ EAT_BO/Models/UserModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EAT_BO.Models
+{
+    public class UserFirebaseModel
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+        public string token { get; set; }
+    }
+}
